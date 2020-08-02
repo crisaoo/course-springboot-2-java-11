122a9a4789f8849a7ee358a897506089e2cce39b
Crisaoo <Cristianocosta2019@outlook.com>
2020-08-02 16:58:57 -0300

    JPA repository, dependency injection and database seeding

---
+++ src/main/java/com/pescaria/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.pescaria.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pescaria.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+}
