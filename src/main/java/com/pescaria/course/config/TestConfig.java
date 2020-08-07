1633b583a9688a083044c2c1fafa7cdfcfa063fc
Crisaoo <Cristianocosta2019@outlook.com>
2020-08-07 18:58:29 -0300

    Payment and one-to-one association

--- src/main/java/com/pescaria/course/config/TestConfig.java
+++ src/main/java/com/pescaria/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.pescaria.course.entities.Category;
 import com.pescaria.course.entities.Order;
 import com.pescaria.course.entities.OrderItem;
+import com.pescaria.course.entities.Payment;
 import com.pescaria.course.entities.Product;
 import com.pescaria.course.entities.User;
 import com.pescaria.course.entities.enums.OrderStatus;
@@ -64,11 +65,15 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3)); 
-		
+				
 		OrderItem oi1 = new OrderItem(o1, p1, 2);
 		OrderItem oi2 = new OrderItem(o1, p3, 1);
 		OrderItem oi3 = new OrderItem(o2, p3, 2);
 		OrderItem oi4 = new OrderItem(o3, p5, 2); 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);		// Não criamos um repository para uma classe dependente (Pois um pedido pode ainda não ter um pagamento)
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/pescaria/course/entities/Order.java
+++ src/main/java/com/pescaria/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -33,6 +35,8 @@ public class Order implements Serializable{
 	private User client;
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)	// CascadeType.ALL = para que o payment tenha o mesmo id do order
+	private Payment payment;
 	
 	
 	public Order() {}
@@ -82,6 +86,14 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	
 	@Override
 	public int hashCode() {
